2fd2ad84d0dfaaf5903396e0f5245e0fbb4349fb
Maikol <maikolsoro.z1998@gmail.com>
2023-08-14 12:19:05 -0700

    Added Section of Friends(More friends and Mutal friends)

--- app/src/main/java/com/example/socialmedia/ui/screens/user_profile/UserProfileScreen.kt
+++ app/src/main/java/com/example/socialmedia/ui/screens/user_profile/UserProfileScreen.kt
@@ -1,28 +1,17 @@
 package com.example.socialmedia.ui.screens.user_profile
 
-import android.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.rounded.Message
 import androidx.compose.material.icons.rounded.MoreHoriz
-import androidx.compose.material.icons.rounded.People
 import androidx.compose.material.icons.rounded.PersonAddAlt
 import androidx.compose.material.icons.rounded.Textsms
 import androidx.compose.material3.Badge
@@ -35,21 +24,18 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -58,6 +44,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.SubcomposeAsyncImage
 import com.example.socialmedia.data.models.User
 import com.example.socialmedia.ui.screens.home.CommonButton
+import com.example.socialmedia.ui.screens.home.StoryCard
 import com.example.socialmedia.ui.theme.SocialMediaTheme
 import com.example.socialmedia.ui.theme.facebookBlue
 import com.example.socialmedia.ui.theme.facebookDarkGray
@@ -210,7 +197,90 @@ fun UserProfileScreen(userProfileViewModel: UserProfileViewModel = hiltViewModel
                 }
             }
 
-            // Friends
+            item {
+                //Friends
+
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(20.dp)
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 22.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Column {
+                            Text(
+                                text = "Friends",
+                                style = MaterialTheme.typography.titleLarge,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(
+                                text = "${currentUser.friends.size} - 10 Mutual friends",
+                                style = MaterialTheme.typography.labelMedium,
+                                color = facebookDarkGray
+                            )
+                        }
+
+                        CommonButton(
+                            text = "See all friends",
+                            color = facebookGray,
+                            tintColor = MaterialTheme.colorScheme.onBackground
+                        ) {
+
+                        }
+
+                    }
+
+                    LazyRow(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(4.dp)
+                    ) {
+
+                        item {
+                            Spacer(modifier = Modifier.size(15.dp))
+                        }
+                        //Mutual Friends
+                        items(currentUser.friends) {
+                            AvatarImg(
+                                avatar = it.avatar,
+                                modifier = Modifier
+                                    .size(50.dp)
+                                    .shadow(elevation = 8.dp, shape = CircleShape)
+                            )
+                        }
+                    }
+                }
+            }
+
+            item {
+                LazyRow(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+
+                    item {
+                        Spacer(modifier = Modifier.size(15.dp))
+                    }
+                    //More friends
+                    items(currentUser.friends) {
+                        StoryCard(
+                            textColor = MaterialTheme.colorScheme.onBackground,
+                            avatarImage = it.avatar,
+                            title = it.name,
+                            bannerImage = it.bannerImage
+                        )
+                    }
+                }
+            }
+
+            item {
+                Spacer(modifier = Modifier.size(20.dp))
+            }
         }
     }
 }
